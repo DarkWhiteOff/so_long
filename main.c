313222bc72e3ecde2d32de53e43e8b19fc689bfa
Ziggy Amgar <zamgar@made-f0Dr5s2.clusters.42paris.fr>
2024-09-02 18:46:02 +0200

    main changes

--- main.c
+++ main.c
@@ -55,8 +55,40 @@ typedef struct s_main {
 	t_sprite	spr_you_win;
 }	t_main;
 
-void    free_what_to_free(t_map *map);
-void    free_rest(t_map *map);
+//void	ft_freezer(t_main *main);
+//void    free_what_to_free(t_map *map);
+//void    free_rest(t_map *map);
+
+/* void	ft_freezer(t_main *main)
+{
+
+}
+
+void	free_what_to_free(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map->height)
+	{
+		free(map->highlight_grid[i]);
+		i++;
+	}
+	free(map->highlight_grid);
+}
+
+void    free_rest(t_map *map)
+{
+        int     i;
+
+        i = 0;
+        while (i < map->height)
+        {
+                free(map->grid[i]);
+                i++;
+        }
+        free(map->grid);
+} */
 
 int     check_map_name(char *map_name)
 {
@@ -92,6 +124,11 @@ size_t  ft_strlenmap(char *line)
         i = 0;
         while (line[i] != '\n' && line[i] != '\0')
                 i++;
+		if (i > 53)
+		{
+			free(line);
+			exit (ft_printf("Error\nYour map is not rectangular or there's nothing in it.\n"));
+		}
         return (i);
 }
 
@@ -105,26 +142,37 @@ void    parse_map(t_map *map, t_pxy *p_pos)
 	line_samelen = 0;
         fd = open(map->path, O_RDONLY);
 	if (fd < 0 || read(fd, 0, 0) < 0)
+	{
+		close(fd); // pas test
 		exit (ft_printf("Error\nfd not working."));
+	}
         line = get_next_line(fd);
 	if (line[0] == '\n' || line[0] == '\0')
+	{
+		free(line); // pas test
 		exit (ft_printf("Error\nYour map has one or more empty lines.\n"));
+	}
 	map->width = ft_strlenmap(line);
         while (line)
         {
-                if (ft_strlenmap(line) != map->width)
+                if (ft_strlenmap(line) != map->width || ft_strlenmap(line) > 53)
                         line_samelen = 1;
                 map->height++;
 		if (line[0] == '\n' || line[0] == '\0')
+		{
+			free(line);
 			exit (ft_printf("Error\nYour map has one or more empty lines.\n"));
-                line = get_next_line(fd);
+		}
+		free(line);
+        line = get_next_line(fd);
 		i++;
         }
         close(fd);
-	free(line);
+		free (line);
+		
 //      ft_printf("map width : %d\n", map->width); // à enlever
 //      ft_printf("map height : %d\n\n", map->height); // à enlever
-        if (map->width == map->height || map->width == 0 || map->height == 0 || line_samelen == 1)
+        if (map->width == map->height || map->width == 0 || map->height == 0 || line_samelen == 1 || map->height > 28)
                exit (ft_printf("Error\nYour map is not rectangular or there's nothing in it.\n"));
 }
 
@@ -202,8 +250,6 @@ void    check_walls1(t_map *map)
         {
                 if (map->grid[0][i] != '1')
 		{
-			free_what_to_free(map);
-			free_rest(map);
                         exit (ft_printf("Error\nYour map is not fully enclosed !\n"));
 		}
                 i++;
@@ -213,8 +259,6 @@ void    check_walls1(t_map *map)
         {
                 if (map->grid[map->height - 1][i] != '1')
 		{
-			free_what_to_free(map);
-			free_rest(map);
                         exit (ft_printf("Error\nYour map is not fully enclosed !\n"));
 		}
                 i++;
@@ -230,14 +274,10 @@ void    check_walls2(t_map *map)
         {
                 if (map->grid[i][0] != '1')
 		{
-			free_what_to_free(map);
-			free_rest(map);
                         exit (ft_printf("Error\nYour map is not fully enclosed !\n"));
 		}
                 if (map->grid[i][map->width - 1] != '1')
 		{
-			free_what_to_free(map);
-			free_rest(map);
                         exit (ft_printf("Error\nYour map is not fully enclosed !\n"));
 		}
                 i++;
@@ -277,8 +317,6 @@ void    check_epc(t_map *map, t_pxy *p_pos, t_pxy *e_pos)
 //      ft_printf("map Collectibles : %d\n\n", map->Coll); // à enlever
         if (map->Ex != 1 || map->Pos != 1 || map->Coll < 1)
 	{
-		free_what_to_free(map);
-		free_rest(map);
 		exit (ft_printf("Error\nVerify that there are only one Exit, one Position and at least one Collectible!\n"));
 	}
 }
@@ -299,19 +337,6 @@ void    check_path(t_map *map, size_t x, size_t y)
         return ;
 }
 
-void	free_what_to_free(t_map *map)
-{
-	int	i;
-
-	i = 0;
-	while (i < map->height)
-	{
-		free(map->highlight_grid[i]);
-		i++;
-	}
-	free(map->highlight_grid);
-}
-
 void	sprites_init(t_main *main)
 {
 	main->spr_wall.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_wall.xpm", &main->spr_wall.width, &main->spr_wall.height);
@@ -320,11 +345,14 @@ void	sprites_init(t_main *main)
 	main->spr_coll2.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_coll2.xpm", &main->spr_coll2.width, &main->spr_coll2.height);
 	main->spr_door_close.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_door_close.xpm", &main->spr_door_close.width, &main->spr_door_close.height);
 	main->spr_player_front.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_player_front.xpm", &main->spr_player_front.width, &main->spr_player_front.height);
-	if (!main->spr_player_front.img)
-		ft_printf("FAILED");
-	main->spr_player_back.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_player_back.xpm", &main->spr_player_back.width, &main->spr_player_back.height);
-	main->spr_player_side.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_player_side.xpm", &main->spr_player_side.width, &main->spr_player_side.height);
-	main->spr_you_win.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_you_win.xpm", &main->spr_you_win.width, &main->spr_you_win.height);
+	//main->spr_player_back.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_player_back.xpm", &main->spr_player_back.width, &main->spr_player_back.height);
+	//main->spr_player_side.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_player_side.xpm", &main->spr_player_side.width, &main->spr_player_side.height);
+	//main->spr_you_win.img = mlx_xpm_file_to_image(main->mlx_ptr, "sprites_tiles/32x32/spr_you_win.xpm", &main->spr_you_win.width, &main->spr_you_win.height);
+	if (!main->spr_player_front.img || !main->spr_wall.img || !main->spr_floor.img || !main->spr_floor_coll.img || !main->spr_coll2.img || !main->spr_door_close.img)
+	{
+		ft_printf("Error : FAILED");
+		close_window(main);
+	}
 }
 
 int     close_window(t_main *main)
@@ -402,19 +430,6 @@ int     key_manager(int keycode, t_main *main)
         return (0);
 }
 
-void    free_rest(t_map *map)
-{
-        int     i;
-
-        i = 0;
-        while (i < map->height)
-        {
-                free(map->grid[i]);
-                i++;
-        }
-        free(map->grid);
-}
-
 void	put_background(t_main *main)
 {
 	int	i = 0;
@@ -438,7 +453,6 @@ void	put_background(t_main *main)
 				mlx_put_image_to_window(main->mlx_ptr, main->mlx_win, main->spr_door_close.img, px_w, px_h);
 			if (main->map.player_on_exit == 1)
 			{
-				free_rest(&main->map);
 				exit (ft_printf("YOU WON THE GAME GG !!!\n"));
 			}
 			j++;
@@ -467,30 +481,30 @@ int	main(int argc, char *argv[])
 		return (ft_printf("Error\nMap name is incorrect.\n"));
 	vars_init(&main.map, argv[1]);
 	parse_map(&main.map, &main.p_pos);
+	//return (0);
 	grid_init(&main);
 	check_walls1(&main.map);
 	check_walls2(&main.map);
 	check_epc(&main.map, &main.p_pos, &main.e_pos);
 	check_path(&main.map, main.p_pos.x, main.p_pos.y);
 	main.map.grid[main.e_pos.y][main.e_pos.x] = '0';
-	free_what_to_free(&main.map);
 //	ft_printf("exit found : %d\n", main.map.exit_acc); // à enlever
 //	ft_printf("collectibles found : %d\n", main.map.coll_acc); // à enlever
 	if (main.map.exit_acc != 1 || main.map.coll_acc != main.map.Coll)
 	{
-		free_rest(&main.map);
 		return (ft_printf("Error\nYour map is invalid because the exit or collectibles aren't accessible.\n"));
 	}
 	main.mlx_ptr = mlx_init();
-	main.mlx_win = mlx_new_window(main.mlx_ptr, main.map.width * 48, main.map.height * 50, "so_long");
-	main.img = mlx_new_image(main.mlx_ptr, main.map.width * 48, main.map.height * 50);
+	if (!main.mlx_ptr)
+		return (1);
+	main.mlx_win = mlx_new_window(main.mlx_ptr, main.map.width * 48, main.map.height * 48, "so_long");
+	if (!main.mlx_win)
+		return (1);
+	main.img = mlx_new_image(main.mlx_ptr, main.map.width * 48, main.map.height * 48);
 	main.addr = mlx_get_data_addr(main.img, &main.bpp, &main.line_size, &main.endian);
 	sprites_init(&main);
 	mlx_hook(main.mlx_win, 2, 1L<<0, key_manager, &main);
 	mlx_hook(main.mlx_win, 17, 0, close_window, &main);
-	//put_background(&main);
-	
-	//mlx_put_image_to_window(main.mlx_ptr, main.mlx_win, main.spr_player_front.img, 0, 0);
 	mlx_loop_hook(main.mlx_ptr, game_refresh, &main);
 	mlx_loop(main.mlx_ptr);
 	return (0);
